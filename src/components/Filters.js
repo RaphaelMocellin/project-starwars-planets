2cae7440d0b591ef16e18912ab9397b772ee4d65
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-29 08:32:41 -0300

    Elaborado e testado o req 7

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -22,72 +22,112 @@ function Filters() {
     setNumericFilter([...numericFilter, newNumericFilter]);
   };
 
+  const onRemoveClick = (column) => {
+    const newNumericFilter = numericFilter.filter((f) => f.columnFilter !== column);
+    setNumericFilter(newNumericFilter);
+  };
+
+  const onRemoveAllClick = () => {
+    setNumericFilter([]);
+  };
+
   useEffect(() => {
     setColumnFilter(columnsArray[0]);
   }, [columnsArray]);
 
   return (
     <div>
-      <label htmlFor="nameFilter">
-        Name Filter:
-        <input
-          id="nameFilter"
-          type="text"
-          name="nameFilter"
-          value={ nameFilter }
-          onChange={ (e) => setNameFilter(e.target.value) }
-          data-testid="name-filter"
-        />
-      </label>
-      <label htmlFor="columnFilter">
-        Coluna:
-        <select
-          id="columnFilter"
-          name="columnFilter"
-          value={ columnFilter }
-          onChange={ (e) => setColumnFilter(e.target.value) }
-          data-testid="column-filter"
+      <div>
+        <label htmlFor="nameFilter">
+          Name Filter:
+          <input
+            id="nameFilter"
+            type="text"
+            name="nameFilter"
+            value={ nameFilter }
+            onChange={ (e) => setNameFilter(e.target.value) }
+            data-testid="name-filter"
+          />
+        </label>
+        <label htmlFor="columnFilter">
+          Coluna:
+          <select
+            id="columnFilter"
+            name="columnFilter"
+            value={ columnFilter }
+            onChange={ (e) => setColumnFilter(e.target.value) }
+            data-testid="column-filter"
+          >
+            {
+              columnsArray.map((col) => (
+                <option key={ col } value={ col }>{ col }</option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="comparisonFilter">
+          Operador:
+          <select
+            id="comparisonFilter"
+            name="comparisonFilter"
+            value={ comparisonFilter }
+            onChange={ (e) => setComparisonFilter(e.target.value) }
+            data-testid="comparison-filter"
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="valueFilter">
+          value:
+          <input
+            id="valueFilter"
+            type="number"
+            name="valueFilter"
+            value={ valueFilter }
+            onChange={ (e) => setValueFilter(e.target.value) }
+            data-testid="value-filter"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ onClickHandler }
+          data-testid="button-filter"
         >
+          Filtrar
+
+        </button>
+        <button
+          type="button"
+          onClick={ onRemoveAllClick }
+          data-testid="button-remove-filters"
+        >
+          Remover Filtros
+
+        </button>
+      </div>
+      <div>
+        <ul>
           {
-            columnsArray.map((col) => (
-              <option key={ col } value={ col }>{ col }</option>
+            numericFilter.map((f) => (
+              <li
+                key={ f.columnFilter }
+                data-testid="filter"
+              >
+                {`${f.columnFilter} ${f.comparisonFilter} ${f.valueFilter} `}
+                <button
+                  type="button"
+                  onClick={ () => onRemoveClick(f.columnFilter) }
+                >
+                  Remover
+
+                </button>
+              </li>
             ))
           }
-        </select>
-      </label>
-      <label htmlFor="comparisonFilter">
-        Operador:
-        <select
-          id="comparisonFilter"
-          name="comparisonFilter"
-          value={ comparisonFilter }
-          onChange={ (e) => setComparisonFilter(e.target.value) }
-          data-testid="comparison-filter"
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <label htmlFor="valueFilter">
-        value:
-        <input
-          id="valueFilter"
-          type="number"
-          name="valueFilter"
-          value={ valueFilter }
-          onChange={ (e) => setValueFilter(e.target.value) }
-          data-testid="value-filter"
-        />
-      </label>
-      <button
-        type="button"
-        onClick={ onClickHandler }
-        data-testid="button-filter"
-      >
-        Filtrar
-
-      </button>
+        </ul>
+      </div>
     </div>
   );
 }
